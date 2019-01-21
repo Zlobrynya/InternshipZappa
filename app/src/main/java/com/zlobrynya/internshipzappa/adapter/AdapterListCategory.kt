bb2d08f7d14c102e54b296d6efb6384891739bad
pakart <obscurexdlol@gmail.com>
2019-01-21 18:47:25 +0400

    Добавлена возможность по нажатию на карточку блюда, открыть информационную сноску

--- app/src/main/java/com/zlobrynya/internshipzappa/adapter/AdapterListCategory.kt
+++ app/src/main/java/com/zlobrynya/internshipzappa/adapter/AdapterListCategory.kt
@@ -2,23 +2,26 @@ package com.zlobrynya.internshipzappa.adapter
 
 import android.content.Context
 import android.os.Build
+import android.support.v7.widget.CardView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.TextView
+import android.widget.*
 import com.zlobrynya.internshipzappa.R
 import com.zlobrynya.internshipzappa.tools.json.Dish
-import android.widget.ImageView
-
 
 
 class AdapterListCategory(context: Context?, resource: Int) :
     ArrayAdapter<Dish>(context, resource) {
 
     class Holder{
-        var nameDish: TextView? = null
-        var imageView: ImageView? = null
+        var buttonDish: Button? = null
+        var imageView: Button? = null
+        var descDish: TextView? = null
+        var priceDish: TextView? = null
+        var weightDish: TextView? = null
+        var shapeDish: CardView? = null
+
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
@@ -29,17 +32,29 @@ class AdapterListCategory(context: Context?, resource: Int) :
             holder = view.tag as Holder
         }else{
             val vi = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            view = vi.inflate(R.layout.item_category_menu, null)
-            holder.nameDish = view.findViewById(R.id.nameDish)
-            holder.imageView = view.findViewById(R.id.imageView)
+            view = vi.inflate(R.layout.full_item_category_menu, null)
+            holder.buttonDish = view.findViewById(R.id.dishButton)
+            holder.imageView = view.findViewById(R.id.dishButton)
+            holder.descDish = view.findViewById(R.id.descDish)
+            holder.priceDish = view.findViewById(R.id.priceDish)
+            holder.weightDish = view.findViewById(R.id.weightDish)
+            holder.shapeDish = view.findViewById(R.id.shapeDish)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 holder.imageView!!.clipToOutline = true
             }
             view.tag = holder
         }
 
-        val dish = getItem(position)
-        holder.nameDish!!.text = dish?.name
+        var dish = getItem(position)
+        holder.buttonDish!!.text = dish?.name
+        holder.descDish!!.text = dish?.descr
+        holder.weightDish!!.text = dish?.weight
+        holder.priceDish!!.text = dish?.price.toString() + " руб."
+        holder.buttonDish!!.setOnClickListener {
+            if(holder.shapeDish!!.visibility == View.VISIBLE) holder.shapeDish!!.visibility = View.GONE
+            else
+            holder.shapeDish!!.visibility = View.VISIBLE
+        }
         return view!!
     }
 }
