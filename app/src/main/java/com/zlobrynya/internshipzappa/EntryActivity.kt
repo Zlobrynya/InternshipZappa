4249fd617232b8bc6ae6d1cb8829aad93c8c3834
pakart <obscurexdlol@gmail.com>
2019-01-24 04:12:40 +0400

    логин и потуги с sharedPreferences

--- app/src/main/java/com/zlobrynya/internshipzappa/EntryActivity.kt
+++ app/src/main/java/com/zlobrynya/internshipzappa/EntryActivity.kt
@@ -4,21 +4,23 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class EntryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry)
-    }
+        val btn = findViewById<Button>(R.id.btnEnter)
 
-    fun ChangeActivityReg(view: View){
-        val change_activity = Intent(this, RegActivity::class.java)
-        startActivity(change_activity)
-    }
+        btn.setOnClickListener {
+            val email = findViewById<EditText>(R.id.etEmailEntry)
+            val pass = findViewById<EditText>(R.id.etPasswordEntry)
+            val entryData = "login=" + email.text + "&password=" + pass.text
+            PostLoginData.getInstance().postServer(entryData)
 
-    fun ChangeActivityMenu(view: View){
-        val change_activity = Intent(this, MainActivity::class.java)
-        startActivity(change_activity)
+
+        }
     }
 }
