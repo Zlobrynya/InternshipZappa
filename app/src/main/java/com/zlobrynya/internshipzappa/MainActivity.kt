4c3af10b290166ce0ed4e93b841aa89529e8d105
Zlobrynya <zlobrynya@gmail.com>
2019-02-22 17:39:35 +0300

    Merge branch 'master' of E:\andorid\InternshipZappa with conflicts.

--- app/src/main/java/com/zlobrynya/internshipzappa/MainActivity.kt
+++ app/src/main/java/com/zlobrynya/internshipzappa/MainActivity.kt
@@ -2,11 +2,27 @@ package com.zlobrynya.internshipzappa
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Создание RecyclerView
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.setLayoutManager(layoutManager)
+        recyclerView.adapter = Adapter(generateValues())
     }
+
+    private fun generateValues(): List<String> {
+        val values = mutableListOf<String>()
+        for (i in 0..3){
+            values.add("Медово-горчичный соус, $i")
+        }
+        return values
+    }
+
 }
