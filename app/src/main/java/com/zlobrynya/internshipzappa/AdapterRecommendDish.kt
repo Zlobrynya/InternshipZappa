c03c95b6d892bf8d539799dfb29d3955b30853b1
FedoseevIlya <Ilafedoseev@mail.ru>
2019-02-23 23:53:08 +0300

    Работает RecyclerView и UIL(спасибо Никите).

--- app/src/main/java/com/zlobrynya/internshipzappa/AdapterRecommendDish.kt
+++ app/src/main/java/com/zlobrynya/internshipzappa/AdapterRecommendDish.kt
@@ -4,7 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.nostra13.universalimageloader.core.ImageLoader
+import kotlinx.android.synthetic.main.full_description_screen.*
 
 class AdapterRecommendDish(private val values: List<String>): RecyclerView.Adapter<AdapterRecommendDish.ViewHolder>() {
 
@@ -17,12 +20,17 @@ class AdapterRecommendDish(private val values: List<String>): RecyclerView.Adapt
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder?.textView?.text = values[position]
+        val imageLoader: ImageLoader = ImageLoader.getInstance()
+        var imageUrl = "drawable://" + R.drawable.noimage
+        imageLoader.displayImage(imageUrl, holder.toping_photo)
     }
 
     class ViewHolder(itemView: View?): RecyclerView.ViewHolder(itemView!!){
         var textView: TextView? = null
+        var toping_photo: ImageView? = null
         init{
             textView = itemView?.findViewById(R.id.topingName)
+            toping_photo = itemView?.findViewById(R.id.topingPhoto)
         }
     }
 }
