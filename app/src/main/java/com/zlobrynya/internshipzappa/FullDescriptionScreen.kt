16b7c784778475fb973f99b77c50bbd0e7cbb584
FedoseevIlya <Ilafedoseev@mail.ru>
2019-02-23 04:28:23 +0300

    Не работает RecyclerView и UIL, без слияния с БД(утром).

--- app/src/main/java/com/zlobrynya/internshipzappa/FullDescriptionScreen.kt
+++ app/src/main/java/com/zlobrynya/internshipzappa/FullDescriptionScreen.kt
@@ -1,20 +1,39 @@
 package com.zlobrynya.internshipzappa
 
+import android.graphics.Bitmap
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import com.nostra13.universalimageloader.core.DisplayImageOptions
+import com.nostra13.universalimageloader.core.ImageLoader
+import com.nostra13.universalimageloader.core.assist.FailReason
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener
+import kotlinx.android.synthetic.main.full_description_screen.*
+import kotlinx.android.synthetic.main.rect_recommend_dish.*
 
 class FullDescriptionScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.full_description_screen)
+
         //Создание RecyclerView
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.setLayoutManager(layoutManager)
         recyclerView.adapter = AdapterRecommendDish(generateValues())
+
+        //UIL
+        val imageLoader: ImageLoader = ImageLoader.getInstance()
+        var imageUrl = "drawable://" + R.drawable.noimage
+        imageLoader.displayImage(imageUrl, dishPhoto)
+        imageLoader.displayImage(imageUrl, topingPhoto)
     }
 
     private fun generateValues(): List<String> {
--- app/src/main/java/com/zlobrynya/internshipzappa/MainActivity.kt
+++ app/src/main/java/com/zlobrynya/internshipzappa/MainActivity.kt
@@ -2,12 +2,32 @@ package com.zlobrynya.internshipzappa
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.zlobrynya.internshipzappa.R
+import com.nostra13.universalimageloader.core.ImageLoader
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator
+import com.nostra13.universalimageloader.cache.disc.impl.LimitedAgeDiskCache
+import com.nostra13.universalimageloader.cache.memory.impl.UsingFreqLimitedMemoryCache
+import com.nostra13.universalimageloader.core.download.ImageDownloader
+import java.io.InputStream
 
-class MainActivity : AppCompatActivity() {
+import java.security.AccessController.getContext
+
+
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.full_description_screen)
+
+        //UIL config
+        val config = ImageLoaderConfiguration.Builder(this)
+            .threadPoolSize(3)
+            .denyCacheImageMultipleSizesInMemory()
+            .memoryCache(UsingFreqLimitedMemoryCache(8 * 1024 * 1024)) // 2 Mb
+            .diskCache(LimitedAgeDiskCache(cacheDir, null, HashCodeFileNameGenerator(), (60 * 60 * 30).toLong()))
+            .imageDownloader(BaseImageDownloader(this)) // connectTimeout (5 s), readTimeout (30 s)
+            .build()
+        ImageLoader.getInstance().init(config)
     }
 }
