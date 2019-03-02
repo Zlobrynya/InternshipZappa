f1f78919af474c0298331862207da99c487f0c52
Zlobrynya <zlobrynya@gmail.com>
2019-03-02 10:23:12 +0300

    Исправлен вылет.

--- app/src/main/java/com/zlobrynya/internshipzappa/activity/MenuActivity.kt
+++ app/src/main/java/com/zlobrynya/internshipzappa/activity/MenuActivity.kt
@@ -7,6 +7,13 @@ import com.zlobrynya.internshipzappa.R
 import com.zlobrynya.internshipzappa.adapter.AdapterTab
 import kotlinx.android.synthetic.main.activity_menu.*
 import android.util.Log
+import com.nostra13.universalimageloader.cache.disc.impl.LimitedAgeDiskCache
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator
+import com.nostra13.universalimageloader.core.DisplayImageOptions
+import com.nostra13.universalimageloader.core.ImageLoader
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
+import com.nostra13.universalimageloader.core.assist.ImageScaleType
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader
 import com.zlobrynya.internshipzappa.tools.database.CategoryDB
 import com.zlobrynya.internshipzappa.tools.retrofit.dto.CatDTO
 
@@ -44,8 +51,25 @@ class MenuActivity: AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onStart() {
+        super.onStart()
+
+        if (!ImageLoader.getInstance().isInited){
+            val options = DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2)
+                .build()
+
+            val config = ImageLoaderConfiguration.Builder(this)
+                .threadPoolSize(5)
+                .diskCache(LimitedAgeDiskCache(cacheDir, null, HashCodeFileNameGenerator(), (60 * 30).toLong()))
+                .imageDownloader(BaseImageDownloader(this)) // connectTimeout (5 s), readTimeout (30 s)
+                .defaultDisplayImageOptions(options)
+                .build()
+            ImageLoader.getInstance().init(config)
+        }
+
     }
 }
 
