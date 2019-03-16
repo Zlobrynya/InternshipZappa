ea8440935dae0d80735fef0bc86f38d953e9a432
Артём Радивилов <radivilovartem@gmail.com>
2019-03-16 23:22:44 +0300

    Вьюпейджер для фрагментов
    Дла запуска поменять в mainactivity интент с menuactivity на menu2activity

---
+++ app/src/main/java/com/zlobrynya/internshipzappa/activity/ViewPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.zlobrynya.internshipzappa.activity
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+import java.util.ArrayList
+
+class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+    private val mFragmentList = ArrayList<Fragment>()
+    override fun getItem(position: Int): Fragment {
+        return mFragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    fun addFragment(fragment: Fragment) {
+        mFragmentList.add(fragment)
+    }
+
+}
