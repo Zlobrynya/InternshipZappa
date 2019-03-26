7e8a6d2bcd172bfe1d2951dd4e576dda80ef6eb2
FedoseevIlya <Ilafedoseev@mail.ru>
2019-03-26 18:57:04 +0300

    Пляски с переходом на нужный фрагмент

--- app/src/main/java/com/zlobrynya/internshipzappa/activity/profile/CodeFEmailActivity.kt
+++ app/src/main/java/com/zlobrynya/internshipzappa/activity/profile/CodeFEmailActivity.kt
@@ -10,6 +10,8 @@ import android.util.Log
 import android.view.View
 import com.zlobrynya.internshipzappa.R
 import com.zlobrynya.internshipzappa.tools.retrofit.DTOs.accountDTOs.authDTO
+import com.zlobrynya.internshipzappa.tools.retrofit.DTOs.accountDTOs.cuserCredentialsDTO
+import com.zlobrynya.internshipzappa.tools.retrofit.DTOs.accountDTOs.passwordRecoveryDTO
 import com.zlobrynya.internshipzappa.tools.retrofit.DTOs.accountDTOs.regDTO
 import com.zlobrynya.internshipzappa.tools.retrofit.DTOs.respDTO
 import com.zlobrynya.internshipzappa.tools.retrofit.RetrofitClientInstance
@@ -30,20 +32,23 @@ class CodeFEmailActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_code_f_email)
 
-        supportActionBar!!.title = "Регистрация"
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setBackgroundDrawable(resources.getDrawable(R.drawable.actionbar))
         supportActionBar!!.elevation = 0F
 
         //принимаем параметры и формируем отсылку
         val newRegister = regDTO()
+        val userCredentials = cuserCredentialsDTO()
+
+        userCredentials.email = intent.getStringExtra("change_email")
+        userCredentials.name = intent.getStringExtra("change_name")
+        userCredentials.phone = intent.getStringExtra("change_phone")
+
         newRegister.email = intent.getStringExtra("email")
         newRegister.code = intent.getStringExtra("code")
         newRegister.password = intent.getStringExtra("password")
         newRegister.name = intent.getStringExtra("name")
         newRegister.phone = intent.getStringExtra("phone")
-
-
 
         firstNumber.addTextChangedListener(object: TextWatcher{
             override fun afterTextChanged(s: Editable?) {
@@ -170,6 +175,35 @@ class CodeFEmailActivity: AppCompatActivity() {
 
         })
 
+        val id = intent.getStringExtra("id")
+
+        when (id){
+            "0"->{
+                supportActionBar!!.title = "Регистрация"
+
+                button2.setOnClickListener(object : View.OnClickListener{
+                    override fun onClick(v: View?) {
+                        code = firstNumber.text.toString() + secondNumber.text.toString() +
+                                thirdNumber.text.toString() + fourthNumber.text.toString() + fifthNumber.text.toString()
+                        Log.d("code", "$code")
+
+                        newRegister.code = code
+
+
+                        Log.i("data", newRegister.email)
+                        Log.i("data", newRegister.code)
+                        Log.i("data", newRegister.phone)
+                        Log.i("data", newRegister.password)
+                        Log.i("data", newRegister.name)
+
+                        postRegister(newRegister)
+                    }
+                })
+            }
+            "1"->{
+                supportActionBar!!.title = "Редактирование профиля"
+            }
+        }
         button2.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
                 code = firstNumber.text.toString() + secondNumber.text.toString() +
@@ -185,42 +219,45 @@ class CodeFEmailActivity: AppCompatActivity() {
                 Log.i("data", newRegister.password)
                 Log.i("data", newRegister.name)
 
-                RetrofitClientInstance.getInstance()
-                    .postRegData(newRegister)
-                    .subscribeOn(Schedulers.io())
-                    ?.observeOn(AndroidSchedulers.mainThread())
-                    ?.subscribe(object : Observer<Response<respDTO>> {
-
-                        override fun onComplete() {}
-
-                        override fun onSubscribe(d: Disposable) {}
-
-                        override fun onNext(t: Response<respDTO>) {
-                            Log.i("checkReg", t.code().toString())
-                            if(t.isSuccessful) {
-                                    allert_text.visibility = View.GONE
-                                    firstNumber.setTextColor(resources.getColor(R.color.white))
-                                    secondNumber.setTextColor(resources.getColor(R.color.white))
-                                    thirdNumber.setTextColor(resources.getColor(R.color.white))
-                                    fourthNumber.setTextColor(resources.getColor(R.color.white))
-                                    fifthNumber.setTextColor(resources.getColor(R.color.white))
-                                Log.i("checkReg", t.body()!!.desc)
-                            }else{
-                                allert_text.visibility = View.VISIBLE
-                                firstNumber.setTextColor(resources.getColor(R.color.color_accent))
-                                secondNumber.setTextColor(resources.getColor(R.color.color_accent))
-                                thirdNumber.setTextColor(resources.getColor(R.color.color_accent))
-                                fourthNumber.setTextColor(resources.getColor(R.color.color_accent))
-                                fifthNumber.setTextColor(resources.getColor(R.color.color_accent))
-                            }
-                        }
-                        override fun onError(e: Throwable) {
-                            Log.i("checkReg", "that's not fineIn")
-                        }
-                    })
+                postRegister(newRegister)
             }
         })
     }
 
+    private fun postRegister(newRegister: regDTO){
+        RetrofitClientInstance.getInstance()
+            .postRegData(newRegister)
+            .subscribeOn(Schedulers.io())
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe(object : Observer<Response<respDTO>> {
+
+                override fun onComplete() {}
+
+                override fun onSubscribe(d: Disposable) {}
+
+                override fun onNext(t: Response<respDTO>) {
+                    Log.i("checkReg", t.code().toString())
+                    if(t.isSuccessful) {
+                        allert_text.visibility = View.GONE
+                        firstNumber.setTextColor(resources.getColor(R.color.white))
+                        secondNumber.setTextColor(resources.getColor(R.color.white))
+                        thirdNumber.setTextColor(resources.getColor(R.color.white))
+                        fourthNumber.setTextColor(resources.getColor(R.color.white))
+                        fifthNumber.setTextColor(resources.getColor(R.color.white))
+                        Log.i("checkReg", t.body()!!.desc)
+                    }else{
+                        allert_text.visibility = View.VISIBLE
+                        firstNumber.setTextColor(resources.getColor(R.color.color_accent))
+                        secondNumber.setTextColor(resources.getColor(R.color.color_accent))
+                        thirdNumber.setTextColor(resources.getColor(R.color.color_accent))
+                        fourthNumber.setTextColor(resources.getColor(R.color.color_accent))
+                        fifthNumber.setTextColor(resources.getColor(R.color.color_accent))
+                    }
+                }
+                override fun onError(e: Throwable) {
+                    Log.i("checkReg", "that's not fineIn")
+                }
+            })
+    }
 
 }
